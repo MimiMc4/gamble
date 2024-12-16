215512bb2abc65b28f835a0f4d4a504727cb17e9
Miguel Martínez <150590491+MimiMc4@users.noreply.github.com>
2024-12-16 16:24:45 +0100

    Add files via upload

---
+++ gamble.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+const string symbols[] = {"\033[0;31m♥\033[0;0m","♠","♣","\033[0;31m♦\033[0;0m","❼"};
+const string slot1 = "--(Gamble!)--\n\\ [";
+const string slot2 = "] /\n/===========\\";
+
+
+void printMsg(string s1, string s2, string s3){
+
+    if(s1 == s2 && s2 == s3 && s1 == "❼"){
+        cout << "\033[5;0m\n777 777 777 777\nPREMIO PREMIO PREMIO PREMIO\nAFIRMAR AFIRMAR AFIRMAR AFIRMAR\n🤑 🤑 🤑 🤑";
+    }else if(s1 == s2 && s2 == s3){
+        cout << "\033[5;0m\nPREMIO PREMIO PREMIO PREMIO";
+    }else if(s1 == s2 || s1 == s3 || s2 == s3){
+        cout << "\ncasi, pero no...";
+    }else{
+        cout << "\nX X X oh dang it";
+    }
+
+
+}
+
+int main(){
+  
+    struct timespec t= { 0, 90000000 };
+
+    srand(time(NULL));
+
+    string s1,s2,s3;
+    int i1,i2,i3;
+    i1 = rand()%5;
+    i2 = rand()%5;
+    i3 = rand()%5;
+    for(int i = 0; i < 10; i++){
+        s1 = symbols[i1++%5];
+        s2 = symbols[i2++%5];
+        s3 = symbols[i3++%5];
+
+        cout << "| [" + s1 + "][" + s2 + "][" + s3 + "] |"  ;
+        flush(cout);
+        cout << "\r";
+
+        nanosleep(&t,NULL);
+    }
+    
+    for(int i = 0; i < 7; i++){
+        s2 = symbols[i2++%5];
+        s3 = symbols[i3++%5];
+
+        cout << "| [" + s1 + "][" + s2 + "][" + s3 + "] |"  ;
+        flush(cout);
+        cout << "\r";
+
+        nanosleep(&t,NULL);
+    }
+
+    for(int i = 0; i < 7; i++){
+        s3 = symbols[i3++%5];
+
+        cout << "| [" + s1 + "][" + s2 + "][" + s3 + "] |"  ;
+        flush(cout);
+        cout << "\r";
+
+        nanosleep(&t,NULL);
+    }
+
+    for(int i = 0; i < 4; i++){
+        cout << "| [" + s1 + "][" + s2 + "][" + s3 + "] |";
+        flush(cout);
+
+        cout << "\r";
+
+        nanosleep(&t,NULL);
+        nanosleep(&t,NULL);
+
+        if(i != 3){
+            cout << "| [ ][ ][ ] |"  ;
+            flush(cout);
+
+            cout << "\r";
+
+            nanosleep(&t,NULL);
+            nanosleep(&t,NULL);
+        }
+    }
+    printMsg(s1,s2,s3); 
+    flush(cout);
+
+    nanosleep(&t,NULL);
+    nanosleep(&t,NULL);
+    nanosleep(&t,NULL);
+    nanosleep(&t,NULL);
+    nanosleep(&t,NULL);
+    nanosleep(&t,NULL);
+    nanosleep(&t,NULL);
+    nanosleep(&t,NULL);
+
+    cout << "\n\033[0;0m";
+}
+
+
